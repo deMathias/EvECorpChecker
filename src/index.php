5cce9611cfdae34229fd4d8ad72dcd46df83fbc1
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2023-06-11 18:50:52 +0200

    Style the page and remember previous outputs
    - update php version
    - bug fixes, functions cannot access global vars because php

--- src/index.php
+++ src/index.php
@@ -14,16 +14,36 @@ if (isset($_POST['names'])) {
 <html>
 <head>
     <title>EVE Online Characters</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
 </head>
-<body>
-    <form method="post">
-        <textarea name="names" placeholder="Enter character names separated by a newline"></textarea><br>
-        <input type="submit" value="Submit">
-    </form>
-    <?php
-    if (!empty($matchingNames)) {
-        echo '<p>Matching characters: '.implode('<br>', $matchingNames).'</p>';
-    }
-    ?>
+<body class="bg-dark text-white">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 text-center mt-5 mb-4">
+                <h1 class="display-4">EvE Corps Checker</h1>
+            </div>
+        </div>
+        <form method="post" class="row">
+            <div class="col-6">
+                <textarea name="names" class="form-control my-2" placeholder="Enter character names separated by a newline" rows="20"></textarea>
+            </div>
+            <div class="col-6">
+                <textarea name="output" class="form-control my-2"rows="20" readonly><?php
+                    if (!empty($matchingNames)) {
+                        echo implode("\n", $matchingNames);
+                    }
+                    if (!empty($_POST['output'])) {
+                        echo "\n--\n" . $_POST['output'];
+                    }
+                ?></textarea>
+            </div>
+            <div class="col-12">
+                <input type="submit" value="Submit" class="btn btn-success btn-lg btn-block my-2">
+            </div>
+        </form>
+    </div>
+    
 </body>
 </html>
+
--- src/lib.php
+++ src/lib.php
@@ -2,33 +2,6 @@
 require 'vendor/autoload.php';
 use GuzzleHttp\Client;
 
-// Computer owned coorperations
-$corporations = [
-    "Hedion University",
-    "Imperial Academy",
-    "Royal Amarr Institute",
-    "School of Applied Knowledge",
-    "Science and Trade Institute",
-    "State War Academy",
-    "Center for Advanced Studies",
-    "Federal Navy Academy",
-    "University of Caille",
-    "Pator Tech School",
-    "Republic Military School",
-    "Republic University",
-    "Viziam",
-    "Ministry of War",
-    "Imperial Shipment",
-    "Perkone",
-    "Caldari Provisions",
-    "Deep Core Mining Inc.",
-    "The Scope",
-    "Aliastra",
-    "Garoun Investment Bank",
-    "Brutor Tribe",
-    "Sebiestor Tribe",
-    "Native Freshfood"
-];
 
 $client = new Client([
     'base_uri' => 'https://esi.evetech.net/latest/',
@@ -37,8 +10,9 @@ $client = new Client([
 
 
 function checkCorps($names) {
+    $client = getClient();
+    $corporations = getComputerCorps();
     $matchingNames = [];
-    var_dump($names);
     foreach ($names as $name) {
         $name = trim($name);
         $response = $client->post('universe/ids/', ['json' => [$name]]);
@@ -59,6 +33,42 @@ function checkCorps($names) {
         }
     }
     return $matchingNames;
+}
+
+function getClient() {
+    return new Client([
+        'base_uri' => 'https://esi.evetech.net/latest/',
+        'timeout'  => 2.0,
+    ]);
+}
+
+function getComputerCorps() {
+    return [
+        "Hedion University",
+        "Imperial Academy",
+        "Royal Amarr Institute",
+        "School of Applied Knowledge",
+        "Science and Trade Institute",
+        "State War Academy",
+        "Center for Advanced Studies",
+        "Federal Navy Academy",
+        "University of Caille",
+        "Pator Tech School",
+        "Republic Military School",
+        "Republic University",
+        "Viziam",
+        "Ministry of War",
+        "Imperial Shipment",
+        "Perkone",
+        "Caldari Provisions",
+        "Deep Core Mining Inc.",
+        "The Scope",
+        "Aliastra",
+        "Garoun Investment Bank",
+        "Brutor Tribe",
+        "Sebiestor Tribe",
+        "Native Freshfood"
+    ];
 }
 
 ?>
